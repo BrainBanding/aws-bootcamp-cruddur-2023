d18e5a3af940861b5a6dc4ac524a5b7cc4afbc4e
Gennaro <wizman09@gmail.com>
2023-03-08 05:51:38 +0000

    Implementing RollBar-Errors

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,12 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#ROLLBAR --------------
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 #HONECOMB ------------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -46,6 +52,33 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#ROLLBAR ----
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+#ROLL BAR TEST ----
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
